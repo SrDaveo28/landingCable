1ebfb90ec2efdca02660902a8d734960813cba8e
Srdaveo28 <76437512+SrDaveo28@users.noreply.github.com>
2021-10-30 10:49:18 -0300

    Primer commit

---
+++ initFirebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: " AIzaSyALpt3EnPcB5dXNuNubeUR4wgfiL13kPhw ",
+    authDomain: "cabledigital-dd58d.firebaseapp.com",
+    databaseURL: "https://cabledigital-dd58d.firebaseio.com",
+    projectId: "cabledigital-dd58d",
+    storageBucket: "cabledigital-dd58d.appspot.com",
+    messagingSenderId: "795937108744"
+};
+firebase.initializeApp(firebaseConfig);
