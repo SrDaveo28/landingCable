d85e734676ea767a4b509a8b2e66f7560a216e39
Srdaveo28 <76437512+SrDaveo28@users.noreply.github.com>
2022-02-11 09:25:57 -0300

    proyecto final

--- initFirebase.js
+++ initFirebase.js
@@ -1,9 +1,17 @@
 var firebaseConfig = {
-    apiKey: " AIzaSyALpt3EnPcB5dXNuNubeUR4wgfiL13kPhw ",
-    authDomain: "cabledigital-dd58d.firebaseapp.com",
-    databaseURL: "https://cabledigital-dd58d.firebaseio.com",
-    projectId: "cabledigital-dd58d",
-    storageBucket: "cabledigital-dd58d.appspot.com",
-    messagingSenderId: "795937108744"
+    apiKey: "AIzaSyANltVAFNugqFLDCIuqzzdiyj0t6HlHgs0",
+
+    authDomain: "cableweb-1c57f.firebaseapp.com",
+
+    databaseURL: "https://cableweb-1c57f-default-rtdb.firebaseio.com",
+
+    projectId: "cableweb-1c57f",
+
+    storageBucket: "cableweb-1c57f.appspot.com",
+
+    messagingSenderId: "632509481648",
+
+    appId: "1:632509481648:web:45ad0de8b93d9b7df8b175"
+
 };
 firebase.initializeApp(firebaseConfig);
